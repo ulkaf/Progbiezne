b40c47b3f883db68d3ec69e1aa6a187c4d8b8da3
KamilPresler <kamilpfcb@gmail.com>
2022-03-24 20:19:07 +0100

    Testy

--- Calculator/Test/CalculatorTests.cs
+++ Calculator/Test/CalculatorTests.cs
@@ -1,13 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Test
+namespace Testy
 {
     [TestClass]
-    public class CalculatorTests
+    public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAdd()
         {
+            Calculator.Calculator x = new Calculator.Calculator();
+
+            double y = x.Addition(2, 2);
+
+            Assert.AreEqual(y, 4);
+
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Calculator.Calculator x = new Calculator.Calculator();
+            double y = x.Subtraction(2, 2);
+            Assert.AreEqual(y, 0);
+
+        }
+
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            Calculator.Calculator x = new Calculator.Calculator();
+
+            double y = x.Multiplication(2, 2);
+            Assert.AreEqual(y, 4);
+
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            Calculator.Calculator x = new Calculator.Calculator();
+            double y = x.Division(2, 2);
+            Assert.AreEqual(y, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void DivideException()
+        {
+            Calculator.Calculator x = new Calculator.Calculator();
+            x.Division(2, 0);
         }
     }
 }
