946512ab80a77dd67b527eba68acc26b18ed89ae
UlaFraczek <236526@edu.p.lodz.pl>
2022-03-24 19:58:13 +0100

    zadanie 0

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Nie dziel przez zero");
+            }
+            return a / b;
+
+        }
+
+    }
+}
---
+++ Calculator/Test/CalculatorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
