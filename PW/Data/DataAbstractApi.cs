64c73c1b21a4293de9ba912a7f2a8ba60635242e
UlaFraczek <236526@edu.p.lodz.pl>
2022-04-20 20:03:11 +0200

    DataAbstractApi

---
+++ PW/Data/DataAbstractApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Data
+{
+    public abstract class DataAbstractApi
+    {
+        public abstract int Radius { get; }
+
+        public static DataAbstractApi CreateApi()
+        {
+            return new DataApi();
+        }
+    }
+
+    internal class DataApi : DataAbstractApi
+    {
+        public override int Radius => 100;
+    }
+}
