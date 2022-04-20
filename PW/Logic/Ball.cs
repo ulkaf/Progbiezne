6922e5e1f02c24dfbb0b1bbf2c19223459ed96ea
KamilPresler <kamilpfcb@gmail.com>
2022-04-21 00:27:54 +0200

    XAML+Logika I

---
+++ PW/Logic/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Logic
+{
+    public class Ball
+    {
+        private int size ;
+        private int x;
+        private int y;
+
+        public Ball(int size, int x, int y)
+        {
+            this.size = size;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int Size { get => size; set => size = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+    }
+}
---
+++ PW/Model/Board.cs
@@ -0,0 +1,12 @@
+﻿using Logic;
+using System;
+
+namespace Model
+{
+    public class Board
+    {
+        private Ball ball = new Logic.Ball(10, 300, 300);
+
+        public Ball Ball { get => ball; } 
+    }
+}
