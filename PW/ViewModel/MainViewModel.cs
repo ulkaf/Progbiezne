ac28ae1cf455d90d648282c4e523ca011e1e7d3e
UlaFraczek <236526@edu.p.lodz.pl>
2022-04-21 21:07:04 +0200

    ViewModel

---
+++ PW/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <param name="propertyName">(optional) The name of the property that changed.
+        /// The <see cref="CallerMemberName"/> allows you to obtain the method or property name of the caller to the method.
+        protected virtual void RaisePropertyChange([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  
+        }
+    }
+}
---
+++ PW/ViewModel/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action m_Execute;
+        private readonly Func<bool> m_CanExecute;
+
+        public RelayCommand(Action execute) : this(execute, null) { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.m_Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.m_CanExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.m_CanExecute == null)
+                return true;
+            if (parameter == null)
+                return this.m_CanExecute();
+            return this.m_CanExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            this.m_Execute();
+        }
+    }
+}
